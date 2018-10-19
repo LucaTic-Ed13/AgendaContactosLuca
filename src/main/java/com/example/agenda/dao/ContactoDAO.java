1d5c98b37bdfe7ff6b31fd78960f08811a49e708
irmabf <irblancofdz@gmail.com>
2018-10-19 14:29:49 +0200

    DAO finalizado

--- src/main/java/com/example/agenda/dao/ContactoDAO.java
+++ src/main/java/com/example/agenda/dao/ContactoDAO.java
@@ -1,5 +1,21 @@
 package com.example.agenda.dao;
 
+import java.util.List;
+
+import com.example.agenda.model.Contacto;
+
 public interface ContactoDAO {
+
+	public List<Contacto> list();
+	
+	public Contacto get(int id);
+	
+	public void update(Contacto contacto);
+	
+	public void add(Contacto contacto);
+	
+	public void delete(int id);
+	
+	
 
 }
--- src/main/java/com/example/agenda/dao/ContactoDAOImpl.java
+++ src/main/java/com/example/agenda/dao/ContactoDAOImpl.java
@@ -1,5 +1,39 @@
 package com.example.agenda.dao;
 
+import java.util.List;
+
+import com.example.agenda.model.Contacto;
+
 public class ContactoDAOImpl implements ContactoDAO {
+
+	@Override
+	public List<Contacto> list() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Contacto get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Contacto contacto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add(Contacto contacto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(int id) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
