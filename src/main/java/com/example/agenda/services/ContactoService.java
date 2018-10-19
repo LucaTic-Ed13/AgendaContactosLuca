84fdac4391736bbce0ca9982ee8b262d45e1724a
DavidSkratch <davidlc.91@hotmail.com>
2018-10-19 13:55:58 +0200

    Capa de servicios

--- src/main/java/com/example/agenda/services/ContactoService.java
+++ src/main/java/com/example/agenda/services/ContactoService.java
@@ -1,5 +1,14 @@
 package com.example.agenda.services;
 
+import java.util.List;
+
+import com.ejemplos.spring.model.User;
+
 public interface ContactoService {
 
+	public List<Contacto> list();
+	public Contacto get(int idPersona);
+	public boolean update(Contacto actual);
+	public boolean add(Contacto nuevo);
+	public boolean delete(int idPersona);
 }
--- src/main/java/com/example/agenda/services/ContactoServiceImpl.java
+++ src/main/java/com/example/agenda/services/ContactoServiceImpl.java
@@ -1,5 +1,48 @@
 package com.example.agenda.services;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ejemplos.spring.dao.ContactoDAO;
+import com.ejemplos.spring.model.Contacto;
+
+@Service
+@Transactional
 public class ContactoServiceImpl implements ContactoService {
+	
+	@Autowired
+	private ContactoDAO gestor;
+	
+	@Override
+	public List<Contacto> list() {
+		return gestor.list();
+	}
+
+	@Override
+	public Contacto get(int id) {
+		return gestor.get(id);
+	}
+
+	@Override
+	public boolean update(Contacto actual) {
+		gestor.update(actual);
+		return true;
+	}
+
+	@Override
+	public boolean add(Contacto nuevo) {
+		gestor.add(nuevo));
+		return true;
+	}
+
+	@Override
+	public boolean delete(int id) {
+		gestor.delete(id);
+		return true;
+	}
 
 }
