2332fb3c4ccc5e5ff8b903b9a71cb7dea8d9226c
DavidSkratch <davidlc.91@hotmail.com>
2018-10-22 14:57:56 +0200

    Cambio en DAO

--- src/main/java/com/example/agenda/dao/ContactoDAOImpl.java
+++ src/main/java/com/example/agenda/dao/ContactoDAOImpl.java
@@ -1,15 +1,23 @@
 package com.example.agenda.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
-import com.example.agenda.model.Contacto;
+import javax.persistence.EntityManager;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.agenda.model.*;
 
 public class ContactoDAOImpl implements ContactoDAO {
+	
+	private EntityManager entityManager;
 
+	@SuppressWarnings("unchecked")
 	@Override
-	public List<Contacto> list() {
-		// TODO Auto-generated method stub
-		return null;
+	@Transactional
+	public List<Contacto> list() throws ClassNotFoundException, SQLException {
+		return (List<Contacto>) entityManager.createQuery("FROM personas ORDER BY id_personas").getResultList();
 	}
 
 	@Override
@@ -19,21 +27,21 @@ public class ContactoDAOImpl implements ContactoDAO {
 	}
 
 	@Override
-	public void update(Contacto contacto) {
+	public boolean update(Contacto contacto) {
 		// TODO Auto-generated method stub
-		
+		return true;
 	}
 
 	@Override
-	public void add(Contacto contacto) {
+	public boolean insert(Contacto contacto) {
 		// TODO Auto-generated method stub
-		
+		return true;
 	}
 
 	@Override
-	public void delete(int id) {
+	public boolean delete(int id) {
 		// TODO Auto-generated method stub
-		
+		return true;
 	}
 
 }
