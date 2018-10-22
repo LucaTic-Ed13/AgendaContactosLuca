285a05c82864b6e1e0cf7bda09699a4f6d1fd36f
irmabf <irblancofdz@gmail.com>
2018-10-22 15:36:47 +0200

    Resolver conflicto

--- src/main/java/com/example/agenda/services/ContactoServiceImpl.java
+++ src/main/java/com/example/agenda/services/ContactoServiceImpl.java
@@ -21,10 +21,6 @@ public class ContactoServiceImpl implements ContactoService {
 
 	@Override
 	public List<Contacto> list() {
-<<<<<<< HEAD
-		List <Contacto> contacto = new List<>();
-=======
->>>>>>> 192bcbac5c119e95a36e6bda5cbe78e6c8e57b47
 		try {
 			return gestor.list();
 		} catch (ClassNotFoundException | SQLException e) {
