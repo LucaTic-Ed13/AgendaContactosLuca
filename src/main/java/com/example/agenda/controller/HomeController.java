e3151cd7f180d99abdb118ed30d7ae39edeab14a
irmabf <irblancofdz@gmail.com>
2018-10-22 15:08:41 +0200

    Pruebas

--- src/main/java/com/example/agenda/controller/HomeController.java
+++ src/main/java/com/example/agenda/controller/HomeController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,11 +22,21 @@ public class HomeController {
 	@Autowired
 	private ContactoService contactoService;
 	
-	@RequestMapping("/agenda")
+	
+	
+	
+	/*@RequestMapping("/agenda")
 	public ModelAndView handleRequest() throws Exception {
 		List<Contacto> listContactos = contactoService.list();
 		ModelAndView model = new ModelAndView("ContactoList");
 		model.addObject("contactoList", listContactos);
 		return model;
+		
+	}
+	*/
+	
+	@GetMapping("/")
+	public String home() {
+		return "home";
 	}
 }
