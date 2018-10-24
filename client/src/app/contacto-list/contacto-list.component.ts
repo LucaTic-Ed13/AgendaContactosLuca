33923aa15626c37e59f6f9624dc8b35d512ed8b3
irmabf <irblancofdz@gmail.com>
2018-10-24 17:38:40 +0200

    contact-edit, contact.service

--- client/src/app/contacto-list/contacto-list.component.ts
+++ client/src/app/contacto-list/contacto-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactoService } from '../shared';
+
+
 
 @Component({
   selector: 'app-contacto-list',
   templateUrl: './contacto-list.component.html',
-  styleUrls: ['./contacto-list.component.css']
+  styleUrls: ['./contacto-list.component.css'],
+  providers: [ContactoService]
 })
-export class ContactoListComponent implements OnInit {
 
-  constructor() { }
+export class ContactoListComponent implements OnInit {
+  contactos: Array<any>;
+  constructor(private contactoService: ContactoService){}
 
   ngOnInit() {
+    this.contactoService.getAll().subscribe(
+      data => {
+        this.contactos = data;
+      },
+      error => console.log(error)
+    )
   }
-
 }
+
+
+
+
+
--- client/src/app/shared/contacto/contacto.service.ts
+++ client/src/app/shared/contacto/contacto.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
+
 export class ContactoService {
+  public API = '//localhost:8080';
+  public CONTACTO_API = this.API + '/contactos';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  // LISTADO
+  getAll(): Observable<any> {
+    return this.http.get(this.API + '/lista-contactos');
+  }
 }
+
