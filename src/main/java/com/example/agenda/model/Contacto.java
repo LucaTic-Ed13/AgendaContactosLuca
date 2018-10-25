8d7118dbfb4675a2b62f0835b85f263f460dc778
DavidSkratch <davidlc.91@hotmail.com>
2018-10-25 15:10:47 +0200

    Holi

--- src/main/java/com/example/agenda/model/Contacto.java
+++ src/main/java/com/example/agenda/model/Contacto.java
@@ -11,6 +11,8 @@ import javax.persistence.Embedded;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.MapsId;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 
 
 @Entity
--- src/test/java/com/example/agenda/AgendaContactosApplicationTests.java
+++ src/test/java/com/example/agenda/AgendaContactosApplicationTests.java
@@ -1,9 +1,15 @@
 package com.example.agenda;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.agenda.model.Contacto;
+import com.example.agenda.services.ContactoFactory;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
